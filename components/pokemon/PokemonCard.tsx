57bba18e2adc787ed405ff8235d62ecc85581901
APlouzeau <plouzeau.alexandre@outlook.com>
2025-02-04 19:51:25 +0100

    fix images

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,7 +1,7 @@
 import type { ViewStyle } from "react-native";
 import { ThemedText } from "../ThemedText";
 import { Card } from "@/components/Card";
-import { Image } from "react-native";
+import { StyleSheet, Image } from "react-native";
 
 type Props = {
     style?: ViewStyle;
@@ -11,17 +11,32 @@ type Props = {
 
 export function PokemonCard({ style, id, name }: Props) {
     return (
-        <Card style={style}>
-            <ThemedText variant="caption" color="grayMedium">
+        <Card style={[style, styles.card]}>
+            <ThemedText style={styles.id} variant="caption" color="grayMedium">
                 #{id.toString().padStart(3, "0")}
             </ThemedText>
             <Image
                 source={{
                     uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
                 }}
-                style={{ width: 72, height: 72 }}
+                style={{ width: 120, height: 120 }}
+                onError={(e) => console.log("Erreur chargement image:", e.nativeEvent.error)}
+                onLoad={() => console.log(`Image ${id} chargée`)}
+                testID="pokemon-image"
             />
-            <ThemedText>{name}</ThemedText>
+            <ThemedText style={styles.name}>{name}</ThemedText>
         </Card>
     );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        alignItems: "center",
+    },
+    id: {
+        alignSelf: "flex-start",
+    },
+    name: {
+        alignSelf: "flex-end",
+    },
+});
